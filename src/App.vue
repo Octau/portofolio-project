618e0a4a4f959907c75212eae9aaef135e5afbe3
octable <octavius3080@gmail.com>
2021-01-26 00:05:55 +0700

    Halfway thru navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <b-navbar toggleable="sm" type="light" variant="light" fixed="top">
+    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+    <b-navbar-brand>Octavius Young  |  Portofolio Website</b-navbar-brand>
+
+    <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+
+    <b-carousel
+      id="carousel-1"
+      fade
+      v-model="slide"
+      :interval="3000"
+      background="#ccc"
+      img-width="100%"
+      img-height="100vh"
+      style="text-shadow: 1px 1px 2px #000;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+      </b-carousel-slide>
+    </b-carousel>
+  <b-navbar class="bottom-navbar p-0" toggleable="sm" type="light" variant="light" fixed="bottom">
+    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+    <!-- <b-navbar-brand>BootstrapVue</b-navbar-brand> -->
+
+    <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav v-b-hover="buttonHover">
+        <b-nav-item class="pt-0 mx-auto">ABOUT</b-nav-item>
+        <b-nav-item class="pt-0 mx-auto">ABOUT</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null,
+        isHovered: false,
+      }
+    },
+    methods: {
+      onSlideStart() {
+        this.sliding = true;
+      },
+      onSlideEnd() {
+        this.sliding = false;
+      },
+      buttonHover(hovered) {
+        this.isHovered = hovered;
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "./assets/scss/index.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+
+// Install BootstrapVue icon
+Vue.use(IconsPlugin)
+
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
